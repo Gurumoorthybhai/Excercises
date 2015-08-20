b58c859763c3715cc56739f735c8a15be1332a17
Krzysztof Witczak <kwitczak@soldevelo.com>
2015-08-20 22:49:11 +0200

    Ultra fix for OddOrNot

--- src_f/java67/main/OddOrNot.java
+++ src_f/java67/main/OddOrNot.java
@@ -1,12 +1,8 @@
 package main;
-//
+
 public class OddOrNot {
 
 	public static boolean check(double a){
-		if (a%2 == 0){
-			return true;
-		} else {
-			return false;
-		}
+		return (a%2 == 0);
 	}
 }
